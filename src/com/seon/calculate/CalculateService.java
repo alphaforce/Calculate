0376da631942b2eb01171f087096eff6052f01a6
seonfly <songful@outlook.com>
2014-08-17 19:22:56 +0800

    update

--- src/com/seon/calculate/CalculateService.java
+++ src/com/seon/calculate/CalculateService.java
@@ -1,11 +1,19 @@
 package com.seon.calculate;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.RemoteViews;
 
 public class CalculateService extends IntentService {
-	
+    public static final int NOTIFICATION_ID = 1;
+    private Notification mFlow;
+    Notification.Builder mBuilder;
+    NotificationManager mNotificationManager;
 
 	public CalculateService(String name) {
 		super(name);
@@ -13,7 +21,29 @@ public class CalculateService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String mParam1 = prefs.getString(Params.PARAMS_1, "1");
+        String mParam2 = prefs.getString(Params.PARAMS_2, "2");
+
+
+
 	}
-	
+
+    public void sendNotification() {
+        RemoteViews view_custom;
+        // 先设定RemoteViews
+        view_custom = new RemoteViews(getPackageName(), R.layout.notify_layout);
+        view_custom.setTextViewText(R.id.textView, "流量监控");
+        view_custom.setTextViewText(R.id.textView2, "10M/300M");
+        mBuilder = new Notification.Builder(this);
+        mBuilder.setContent(view_custom).setWhen(System.currentTimeMillis()) // 通知产生的时间，会在通知信息里显示
+                .setTicker("开启流量监控").setPriority(Notification.PRIORITY_DEFAULT)// 设置该通知优先级
+                ;
+        mFlow = mBuilder.build();
+        mFlow.flags = Notification.FLAG_ONGOING_EVENT
+                | Notification.FLAG_NO_CLEAR;
+        mNotificationManager.notify(NOTIFICATION_ID, mFlow);
+    }
+
+
 }
--- src/com/seon/calculate/MainActivity.java
+++ src/com/seon/calculate/MainActivity.java
@@ -4,12 +4,10 @@ import android.R.integer;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.*;
 import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,58 +15,63 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
-	private EditText mParam1EditText;
-	private EditText mParam2EditText;
-	private Button mCalcalateBtn;
-	
-	private BroadcastReceiver mBootStartReceiver;
+    private EditText mParam1EditText;
+    private EditText mParam2EditText;
+    private Button mCalcalateBtn;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    private BroadcastReceiver mBootStartReceiver;
 
-		mParam1EditText = (EditText) findViewById(R.id.param_1);
-		mParam2EditText = (EditText) findViewById(R.id.param_2);
-		mCalcalateBtn = (Button) findViewById(R.id.calcalate_btn);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		mCalcalateBtn.setOnClickListener(new View.OnClickListener() {
+        mParam1EditText = (EditText) findViewById(R.id.param_1);
+        mParam2EditText = (EditText) findViewById(R.id.param_2);
+        mCalcalateBtn = (Button) findViewById(R.id.calcalate_btn);
 
-			@Override
-			public void onClick(View v) {
-				String param1 = mParam1EditText.getText().toString();
-				String param2 = mParam2EditText.getText().toString();
-				if(param1.isEmpty() || param2.isEmpty()){
-					dialog(0);
-				}else{
-					if (Integer.valueOf(param2) == 0) {
-						dialog(1);
-					}else{
-						
-					}
-				}
-			}
-		});
-	}
+        mCalcalateBtn.setOnClickListener(new View.OnClickListener() {
 
-	private void dialog(final int errorCode) {
-		AlertDialog.Builder builder = new Builder(MainActivity.this);
-		String errorTip = null;
-		if(errorCode == 0){
-			errorTip = "参数不能为空，请重新输入";
-		}else if(errorCode == 1){
-			errorTip = "参数2不能为0，请重新输入";
-		}
-		builder.setMessage(errorTip);
-		builder.setTitle("提示");
-		builder.setPositiveButton("确认", new OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-				if(errorCode == 1) mParam2EditText.setText("");
-			}
-		});
-		builder.create().show();
-	}
+            @Override
+            public void onClick(View v) {
+                String param1 = mParam1EditText.getText().toString();
+                String param2 = mParam2EditText.getText().toString();
+                if (param1.isEmpty() || param2.isEmpty()) {
+                    dialog(0);
+                } else {
+                    if (Integer.valueOf(param2) == 0) {
+                        dialog(1);
+                    } else {
+                        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                        prefs.edit().putString(Params.PARAMS_1, param1)
+                                .putString(Params.PARAMS_1, param2)
+                                .commit();
 
+                        Intent service = new Intent(MainActivity.this, CalculateService.class);
+                        startService(service);
+                    }
+                }
+            }
+        });
+    }
+
+    private void dialog(final int errorCode) {
+        AlertDialog.Builder builder = new Builder(MainActivity.this);
+        String errorTip = null;
+        if (errorCode == 0) {
+            errorTip = "参数不能为空，请重新输入";
+        } else if (errorCode == 1) {
+            errorTip = "第二个数字不能为0，请重新输入";
+        }
+        builder.setMessage(errorTip);
+        builder.setTitle("提示");
+        builder.setPositiveButton("确认", new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if (errorCode == 1) mParam2EditText.setText("");
+            }
+        });
+        builder.create().show();
+    }
 }
